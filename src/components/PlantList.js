19d9dd4709c3d208e9230ac3c2b807255b6dcbaa
James Hawk <jph94880@gmail.com>
2023-10-17 17:05:55 -0500

    Appends pList to dom

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PList({plants}) {
+
+  const pList = plants.map((plant) => <PlantCard key={plant.id} plant={plant}/>)
+  // Maps through the plants array of plant objects and passes down props for each card to PlantCard.
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{pList}</ul>
   );
 }
 
-export default PlantList;
+export default PList;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  // Always use a default value of an empty array
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(setPlants) 
+  }, [])
+  // useEffect always takes two arguments: an anonymous callback function, and an empty dependency array.
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
